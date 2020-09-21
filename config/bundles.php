63bd4c246f27390d613497cf8db235fc2e31b898
Jon Cram <webignition@gmail.com>
2020-09-21 15:43:33 +0100

    Create empty symfony application (#17)

---
+++ config/bundles.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
+];
