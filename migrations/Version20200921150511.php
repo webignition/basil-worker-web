a8153f1a49912f2929021570cd4dd4c09c553d50
Jon Cram <webignition@gmail.com>
2020-09-21 17:17:44 +0100

    Tidy up Version20200921150511 (#20)

--- migrations/Version20200921150511.php
+++ migrations/Version20200921150511.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20200921150511 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,10 +16,12 @@ final class Version20200921150511 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE test_configuration (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-                                browser VARCHAR(255) NOT NULL, 
-                                url VARCHAR(255) NOT NULL)
+        $this->addSql('
+            CREATE TABLE test_configuration (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                browser VARCHAR(255) NOT NULL,
+                url VARCHAR(255) NOT NULL
+            )
         ');
         $this->addSql('CREATE UNIQUE INDEX browser_url_idx ON test_configuration (browser, url)');
     }
