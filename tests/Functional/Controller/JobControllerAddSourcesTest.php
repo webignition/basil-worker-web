ba6b0aadebf327eb81aaedec8a244f2bc1c0a892
Jon Cram <webignition@gmail.com>
2020-09-30 12:47:25 +0100

    Tidy up functional job controller tests (#112)

---
+++ tests/Functional/Controller/JobControllerAddSourcesTest.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Controller;
+
+use App\Entity\Job;
+use App\Request\AddSourcesRequest;
+use App\Services\JobStore;
+use App\Services\SourceStore;
+use App\Tests\Functional\AbstractBaseFunctionalTest;
+use App\Tests\Services\BasilFixtureHandler;
+use App\Tests\Services\SourceStoreInitializer;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
+
+class JobControllerAddSourcesTest extends AbstractBaseFunctionalTest
+{
+    private const EXPECTED_SOURCES = [
+        'Test/test1.yml',
+        'Test/test2.yml',
+        'Test/test3.yml',
+    ];
+
+    private JobStore $jobStore;
+    private BasilFixtureHandler $basilFixtureHandler;
+    private SourceStore $sourceStore;
+    private Job $job;
+    private Response $response;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $jobStore = self::$container->get(JobStore::class);
+        self::assertInstanceOf(JobStore::class, $jobStore);
+        if ($jobStore instanceof JobStore) {
+            $this->jobStore = $jobStore;
+        }
+
+        $basilFixtureHandler = self::$container->get(BasilFixtureHandler::class);
+        self::assertInstanceOf(BasilFixtureHandler::class, $basilFixtureHandler);
+        if ($basilFixtureHandler instanceof BasilFixtureHandler) {
+            $this->basilFixtureHandler = $basilFixtureHandler;
+        }
+
+        $sourceStore = self::$container->get(SourceStore::class);
+        self::assertInstanceOf(SourceStore::class, $sourceStore);
+        if ($sourceStore instanceof SourceStore) {
+            $this->sourceStore = $sourceStore;
+        }
+
+        $this->initializeSourceStore();
+
+        $this->job = $this->createJob();
+        self::assertSame([], $this->job->getSources());
+
+        $this->response = $this->getAddSourcesResponse();
+    }
+
+    public function testResponse()
+    {
+        self::assertSame(200, $this->response->getStatusCode());
+        self::assertSame('application/json', $this->response->headers->get('content-type'));
+        self::assertSame('{}', $this->response->getContent());
+    }
+
+    public function testJobHasSources()
+    {
+        self::assertSame(self::EXPECTED_SOURCES, $this->job->getSources());
+    }
+
+    public function testSourcesAreStored()
+    {
+        foreach (self::EXPECTED_SOURCES as $expectedSource) {
+            self::assertTrue($this->sourceStore->has($expectedSource));
+        }
+    }
+
+    private function initializeSourceStore(): void
+    {
+        $sourceStoreInitializer = self::$container->get(SourceStoreInitializer::class);
+        self::assertInstanceOf(SourceStoreInitializer::class, $sourceStoreInitializer);
+        if ($sourceStoreInitializer instanceof SourceStoreInitializer) {
+            $sourceStoreInitializer->initialize();
+        }
+    }
+
+    private function createJob(): Job
+    {
+        $job = Job::create(md5('label content'), 'http://example.com/callback');
+        $this->jobStore->store($job);
+
+        return $job;
+    }
+
+    private function getAddSourcesResponse(): Response
+    {
+        $requestSources = [];
+        foreach (self::EXPECTED_SOURCES as $source) {
+            $requestSources[$source] = $this->basilFixtureHandler->createUploadedFile($source);
+        }
+
+        $requestFiles = array_merge(
+            [
+                AddSourcesRequest::KEY_MANIFEST => new UploadedFile(
+                    getcwd() . '/tests/Fixtures/Manifest/manifest.txt',
+                    'manifest.yml',
+                    'text/yaml',
+                    null,
+                    true
+                ),
+            ],
+            $requestSources
+        );
+
+        $this->client->request(
+            'POST',
+            '/add-sources',
+            [],
+            $requestFiles
+        );
+
+        return $this->client->getResponse();
+    }
+}
--- tests/Functional/Controller/JobControllerTest.php
+++ tests/Functional/Controller/JobControllerTest.php
@@ -6,22 +6,15 @@ namespace App\Tests\Functional\Controller;
 
 use App\Entity\Job;
 use App\Entity\TestConfiguration;
-use App\Request\AddSourcesRequest;
 use App\Request\JobCreateRequest;
 use App\Services\JobStore;
-use App\Services\SourceStore;
 use App\Services\TestStore;
 use App\Tests\Functional\AbstractBaseFunctionalTest;
-use App\Tests\Services\BasilFixtureHandler;
-use App\Tests\Services\SourceStoreInitializer;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class JobControllerTest extends AbstractBaseFunctionalTest
 {
     private JobStore $jobStore;
-    private BasilFixtureHandler $basilFixtureHandler;
-    private SourceStore $sourceStore;
 
     protected function setUp(): void
     {
@@ -31,18 +24,6 @@ class JobControllerTest extends AbstractBaseFunctionalTest
         self::assertInstanceOf(JobStore::class, $jobStore);
         if ($jobStore instanceof JobStore) {
             $this->jobStore = $jobStore;
-        }
-
-        $basilFixtureHandler = self::$container->get(BasilFixtureHandler::class);
-        self::assertInstanceOf(BasilFixtureHandler::class, $basilFixtureHandler);
-        if ($basilFixtureHandler instanceof BasilFixtureHandler) {
-            $this->basilFixtureHandler = $basilFixtureHandler;
-        }
-
-        $sourceStore = self::$container->get(SourceStore::class);
-        self::assertInstanceOf(SourceStore::class, $sourceStore);
-        if ($sourceStore instanceof SourceStore) {
-            $this->sourceStore = $sourceStore;
         }
     }
 
@@ -69,60 +50,6 @@ class JobControllerTest extends AbstractBaseFunctionalTest
             Job::create($label, $callbackUrl),
             $this->jobStore->retrieve()
         );
-    }
-
-    public function testAddSources()
-    {
-        $sourceStoreInitializer = self::$container->get(SourceStoreInitializer::class);
-        self::assertInstanceOf(SourceStoreInitializer::class, $sourceStoreInitializer);
-        if ($sourceStoreInitializer instanceof SourceStoreInitializer) {
-            $sourceStoreInitializer->initialize();
-        }
-
-        $job = Job::create(md5('label content'), 'http://example.com/callback');
-        $this->jobStore->store($job);
-        self::assertSame([], $job->getSources());
-
-        $this->client->request(
-            'POST',
-            '/add-sources',
-            [],
-            [
-                AddSourcesRequest::KEY_MANIFEST => new UploadedFile(
-                    getcwd() . '/tests/Fixtures/Manifest/manifest.txt',
-                    'manifest.yml',
-                    'text/yaml',
-                    null,
-                    true
-                ),
-                'Test/test1.yml' => $this->basilFixtureHandler->createUploadedFile('Test/test1.yml'),
-                'Test/test2.yml' => $this->basilFixtureHandler->createUploadedFile('Test/test2.yml'),
-                'Test/test3.yml' => $this->basilFixtureHandler->createUploadedFile('Test/test3.yml'),
-            ]
-        );
-
-        $response = $this->client->getResponse();
-
-        self::assertSame(200, $response->getStatusCode());
-        self::assertSame('application/json', $response->headers->get('content-type'));
-        self::assertSame('{}', $response->getContent());
-
-        $expectedSources = [
-            'Test/test1.yml',
-            'Test/test2.yml',
-            'Test/test3.yml',
-        ];
-
-        self::assertSame($expectedSources, $job->getSources());
-
-        $retrievedJob = $this->jobStore->retrieve();
-        if ($retrievedJob instanceof Job) {
-            self::assertEquals($expectedSources, $retrievedJob->getSources());
-        }
-
-        foreach ($expectedSources as $expectedSource) {
-            self::assertTrue($this->sourceStore->has($expectedSource));
-        }
     }
 
     /**
