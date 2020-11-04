94c51afeca49fd451006c1c94cce6fc122bcd67d
Jon Cram <webignition@gmail.com>
2020-11-04 16:28:44 +0000

    Set job state to complete only if not complete (#280)

--- src/EventSubscriber/TestExecuteCompleteEventSubscriber.php
+++ src/EventSubscriber/TestExecuteCompleteEventSubscriber.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\EventSubscriber;
 
+use App\Entity\Job;
 use App\Entity\Test;
 use App\Event\TestExecuteCompleteEvent;
 use App\Services\ExecutionWorkflowHandler;
 use App\Services\JobStateMutator;
+use App\Services\JobStore;
 use App\Services\TestStateMutator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -16,15 +18,18 @@ class TestExecuteCompleteEventSubscriber implements EventSubscriberInterface
     private JobStateMutator $jobStateMutator;
     private ExecutionWorkflowHandler $executionWorkflowHandler;
     private TestStateMutator $testStateMutator;
+    private JobStore $jobStore;
 
     public function __construct(
         JobStateMutator $jobStateMutator,
         ExecutionWorkflowHandler $executionWorkflowHandler,
-        TestStateMutator $testStateMutator
+        TestStateMutator $testStateMutator,
+        JobStore $jobStore
     ) {
         $this->jobStateMutator = $jobStateMutator;
         $this->executionWorkflowHandler = $executionWorkflowHandler;
         $this->testStateMutator = $testStateMutator;
+        $this->jobStore = $jobStore;
     }
 
     public static function getSubscribedEvents()
@@ -68,7 +73,9 @@ class TestExecuteCompleteEventSubscriber implements EventSubscriberInterface
 
     public function setJobStateToExecutionCompleteIfAllTestsFinished(): void
     {
-        if ($this->executionWorkflowHandler->isComplete()) {
+        $job = $this->jobStore->getJob();
+
+        if (Job::STATE_EXECUTION_COMPLETE !== $job->getState() && $this->executionWorkflowHandler->isComplete()) {
             $this->jobStateMutator->setExecutionComplete();
         }
     }
