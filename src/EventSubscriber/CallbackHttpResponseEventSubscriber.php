009343f4269e6c8d254634226d50bd5d64f029d0
Jon Cram <webignition@gmail.com>
2020-11-05 10:24:06 +0000

    Create CallbackHttpResponseEventSubscriber (#290)

---
+++ src/EventSubscriber/CallbackHttpResponseEventSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Event\CallbackHttpResponseEvent;
+use App\Message\SendCallback;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class CallbackHttpResponseEventSubscriber implements EventSubscriberInterface
+{
+    private MessageBusInterface $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            CallbackHttpResponseEvent::NAME => [
+                ['dispatchSendCallbackMessage', 0],
+            ],
+        ];
+    }
+
+    public function dispatchSendCallbackMessage(CallbackHttpResponseEvent $event): void
+    {
+        $message = new SendCallback($event->getCallback());
+
+        $this->messageBus->dispatch($message);
+    }
+}
---
+++ tests/Functional/EventSubscriber/CallbackHttpResponseEventSubscriberTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\EventSubscriber;
+
+use App\Event\CallbackHttpResponseEvent;
+use App\EventSubscriber\CallbackHttpResponseEventSubscriber;
+use App\Message\SendCallback;
+use App\Model\Callback\CallbackInterface;
+use App\Tests\AbstractBaseFunctionalTest;
+use App\Tests\Model\TestCallback;
+use GuzzleHttp\Psr7\Response;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Messenger\Transport\InMemoryTransport;
+
+class CallbackHttpResponseEventSubscriberTest extends AbstractBaseFunctionalTest
+{
+    use MockeryPHPUnitIntegration;
+
+    private CallbackHttpResponseEventSubscriber $eventSubscriber;
+    private InMemoryTransport $messengerTransport;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $eventSubscriber = self::$container->get(CallbackHttpResponseEventSubscriber::class);
+        if ($eventSubscriber instanceof CallbackHttpResponseEventSubscriber) {
+            $this->eventSubscriber = $eventSubscriber;
+        }
+
+        $messengerTransport = self::$container->get('messenger.transport.async');
+        if ($messengerTransport instanceof InMemoryTransport) {
+            $this->messengerTransport = $messengerTransport;
+        }
+    }
+
+    public function testDispatchSendCallbackMessage()
+    {
+        $callback = new TestCallback();
+        $response = new Response(503);
+        $event = new CallbackHttpResponseEvent($callback, $response);
+
+        $this->eventSubscriber->dispatchSendCallbackMessage($event);
+
+        $this->assertMessageTransportQueue($callback);
+    }
+
+    public function testIntegration()
+    {
+        self::assertCount(0, $this->messengerTransport->get());
+
+        $callback = new TestCallback();
+        $response = new Response(503);
+        $event = new CallbackHttpResponseEvent($callback, $response);
+
+        $eventDispatcher = self::$container->get(EventDispatcherInterface::class);
+        if ($eventDispatcher instanceof EventDispatcherInterface) {
+            $eventDispatcher->dispatch($event, CallbackHttpResponseEvent::NAME);
+        }
+
+        $this->assertMessageTransportQueue($callback);
+    }
+
+    private function assertMessageTransportQueue(CallbackInterface $expectedCallback): void
+    {
+        $queue = $this->messengerTransport->get();
+        self::assertCount(1, $queue);
+        self::assertIsArray($queue);
+
+        $expectedQueuedMessage = new SendCallback($expectedCallback);
+
+        self::assertEquals($expectedQueuedMessage, $queue[0]->getMessage());
+    }
+}
